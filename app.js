ac1cce18f37735f23e05be0f3355325a24dc6199
Md Tausif Iqbal <mdtausifiqbalnwd@gmail.com>
2023-03-09 16:37:46 +0530

    Challenge 5

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.get("/", function (req, res) {
     res.render("home", { content: homeContent });
 });
 
+app.get("/contact", function (req, res) {
+    res.render("contact", { content: contactContent });
+});
+
+app.get("/about", function (req, res) {
+    res.render("about", { content: aboutContent });
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
